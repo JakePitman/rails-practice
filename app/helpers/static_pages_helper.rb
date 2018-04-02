6b98587a7bca9e16173af49d40ba1e6877a939c7
Jay Pitman <jaypitman@Jays-MBP.gateway>
2018-04-02 20:32:53 +1000

    merge rails-flavored-ruby

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+    def display_params
+        if @params.nil? || @params.empty?
+            "Nothing set for :params"
+        else
+            "Params are: #{@params}"
+        end
+    end
 end
